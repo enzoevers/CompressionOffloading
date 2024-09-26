a7f570f8a4a31d900614e517d934415fb38f441a
Enzo Evers <enzoevers@gmail.com>
2024-09-26 12:29:26 +0200

    dev: inflate with zlib

--- Benchmark/BenchmarkCompressTextFile.c
+++ Benchmark/BenchmarkCompressTextFile.c
@@ -1,6 +1,8 @@
 #include "b63/b63.h"
 #include <CoDeLib/Deflate_zlib/Deflate_zlib.h>
+#include <CoDeLib/Inflate_zlib/Inflate_zlib.h>
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 char *g_pFullPathToBenchmarkTestFiles = NULL;
@@ -19,12 +21,10 @@ RaiiString RaiiStringCreate(size_t length) {
         printf("Failed to allocate memory for RaiiString\n");
         exit(1);
     }
-    // printf("Created RaiiString at: 0x%x\n", newRaiistring.pString);
     return newRaiistring;
 }
 
 void RaiiStringClean(RaiiString *pThis) {
-    // printf("Freeing RaiiString at: 0x%x\n", pThis->pString);
     if (pThis->pString != NULL) {
         free(pThis->pString);
         pThis->pString = NULL;
@@ -49,6 +49,53 @@ void CreateFullPathToFile(const RaiiString *pFullPath,
     }
 }
 
+bool FilesAreEqual(FILE *pFile1, FILE *pFile2) {
+    fseek(pFile1, 0, SEEK_END);
+    const size_t fileSize1 = ftell(pFile1);
+    rewind(pFile1);
+
+    fseek(pFile2, 0, SEEK_END);
+    const size_t fileSize2 = ftell(pFile2);
+    rewind(pFile2);
+
+    if (fileSize1 != fileSize2) {
+        return false;
+    }
+
+    const size_t bufferSize = 1024;
+    char buffer1[bufferSize];
+    char buffer2[bufferSize];
+
+    size_t bytesRead1 = 0;
+    size_t bytesRead2 = 0;
+
+    do {
+        bytesRead1 = fread(buffer1, 1, bufferSize, pFile1);
+        bytesRead2 = fread(buffer2, 1, bufferSize, pFile2);
+
+        if (bytesRead1 != bytesRead2) {
+            return false;
+        }
+
+        const int eofFile1 = feof(pFile1);
+        const int eofFile2 = feof(pFile2);
+        const int errorFile1 = ferror(pFile1);
+        const int errorFile2 = ferror(pFile2);
+        if ((eofFile1 && !eofFile2) || (!eofFile1 && eofFile2)) {
+            printf("EOF indicator set only by one file\n");
+        } else if ((!eofFile1 && errorFile1) || (!eofFile2 && errorFile2)) {
+            printf("Error indicator set");
+            return false;
+        }
+
+        if (memcmp(buffer1, buffer2, bytesRead1) != 0) {
+            return false;
+        }
+    } while (bytesRead1 > 0);
+
+    return true;
+}
+
 size_t GetFileSizeInBytes(FILE *pFile) {
     fseek(pFile, 0, SEEK_END);
     const size_t fileSize = ftell(pFile);
@@ -58,51 +105,100 @@ size_t GetFileSizeInBytes(FILE *pFile) {
 
 B63_BASELINE(basic, n) {
     FILE *pInFile = NULL;
-    FILE *pOutFile = NULL;
+    FILE *pOutCompressedFile = NULL;
+    FILE *pOutDecompressedFile = NULL;
 
     B63_SUSPEND {
-        char *pInFilename = "SmallBasicTextFile.txt";
-        const size_t lengthPathIn =
-            strlen(g_pFullPathToBenchmarkTestFiles) + strlen(pInFilename) + 1;
-        RaiiString fullInPathString __attribute__((cleanup(RaiiStringClean)));
-        fullInPathString = RaiiStringCreate(lengthPathIn);
-        CreateFullPathToFile(&fullInPathString, lengthPathIn,
-                             g_pFullPathToBenchmarkTestFiles, pInFilename);
+        // TODO: Move to separate function and/or simplify
+        {
+            char *pInFilename = "SmallBasicTextFile.txt";
+            const size_t lengthPathIn =
+                strlen(g_pFullPathToBenchmarkTestFiles) + strlen(pInFilename) +
+                1;
+            RaiiString fullInPathString
+                __attribute__((cleanup(RaiiStringClean)));
+            fullInPathString = RaiiStringCreate(lengthPathIn);
+            CreateFullPathToFile(&fullInPathString, lengthPathIn,
+                                 g_pFullPathToBenchmarkTestFiles, pInFilename);
 
-        char *pOutFilename = "SmallBasicTextFile.compressed.txt";
-        const size_t lengthPathOut =
-            strlen(g_pFullPathToBenchmarkTestFiles) + strlen(pOutFilename) + 1;
-        RaiiString fullOutPathString __attribute__((cleanup(RaiiStringClean)));
-        fullOutPathString = RaiiStringCreate(lengthPathOut);
-        CreateFullPathToFile(&fullOutPathString, lengthPathOut,
-                             g_pFullPathToBenchmarkTestFiles, pOutFilename);
-
-        // printf("Opening input file: %s\n", fullInPathString.pString);
-        pInFile = fopen(fullInPathString.pString, "r");
-        if (pInFile == NULL) {
-            printf("Failed to open file: %s\n", fullInPathString.pString);
-            exit(1);
+            pInFile = fopen(fullInPathString.pString, "r");
+            if (pInFile == NULL) {
+                printf("Failed to open file: %s\n", fullInPathString.pString);
+                exit(1);
+            }
         }
 
-        // printf("Opening/Creating output file: %s\n",
-        // fullOutPathString.pString);
-        pOutFile = fopen(fullOutPathString.pString, "w");
-        if (pOutFile == NULL) {
-            printf("Failed to open file: %s\n", fullOutPathString.pString);
-            exit(1);
+        {
+            char *pOutCompressedFilename = "SmallBasicTextFile.compressed.txt";
+            const size_t lengthPathOutCompressed =
+                strlen(g_pFullPathToBenchmarkTestFiles) +
+                strlen(pOutCompressedFilename) + 1;
+            RaiiString fullOutCompressedPathString
+                __attribute__((cleanup(RaiiStringClean)));
+            fullOutCompressedPathString =
+                RaiiStringCreate(lengthPathOutCompressed);
+            CreateFullPathToFile(
+                &fullOutCompressedPathString, lengthPathOutCompressed,
+                g_pFullPathToBenchmarkTestFiles, pOutCompressedFilename);
+
+            pOutCompressedFile =
+                fopen(fullOutCompressedPathString.pString, "w+");
+            if (pOutCompressedFile == NULL) {
+                printf("Failed to open file: %s\n",
+                       fullOutCompressedPathString.pString);
+                exit(1);
+            }
+            assert(GetFileSizeInBytes(pOutCompressedFile) == 0);
         }
-        assert(GetFileSizeInBytes(pOutFile) == 0);
+
+        {
+            char *pOutDecompressedFilename =
+                "SmallBasicTextFile.decompressed.txt";
+            const size_t lengthPathOutDecompressed =
+                strlen(g_pFullPathToBenchmarkTestFiles) +
+                strlen(pOutDecompressedFilename) + 1;
+            RaiiString fullOutDecompressedPathString
+                __attribute__((cleanup(RaiiStringClean)));
+            fullOutDecompressedPathString =
+                RaiiStringCreate(lengthPathOutDecompressed);
+            CreateFullPathToFile(
+                &fullOutDecompressedPathString, lengthPathOutDecompressed,
+                g_pFullPathToBenchmarkTestFiles, pOutDecompressedFilename);
+
+            pOutDecompressedFile =
+                fopen(fullOutDecompressedPathString.pString, "w");
+            if (pOutDecompressedFile == NULL) {
+                printf("Failed to open file: %s\n",
+                       fullOutDecompressedPathString.pString);
+                exit(1);
+            }
+            assert(GetFileSizeInBytes(pOutDecompressedFile) == 0);
+        }
     }
 
-    const DEFLATE_RETURN_CODES status =
-        deflate_zlib.Deflate(pInFile, pOutFile, NULL);
-    B63_KEEP(status);
+    const DEFLATE_RETURN_CODES statusDeflate =
+        deflate_zlib.Deflate(pInFile, pOutCompressedFile, NULL);
+    B63_KEEP(statusDeflate);
 
     B63_SUSPEND {
-        assert(GetFileSizeInBytes(pOutFile) > 0);
-        assert(status == DEFLATE_SUCCESS);
+        assert(GetFileSizeInBytes(pOutCompressedFile) > 0);
+        assert(statusDeflate == DEFLATE_SUCCESS);
+    }
+
+    const INFLATE_RETURN_CODES statusInflate =
+        inflate_zlib.Inflate(pOutCompressedFile, pOutDecompressedFile, NULL);
+    B63_KEEP(statusInflate);
+
+    B63_SUSPEND {
+        assert(GetFileSizeInBytes(pOutDecompressedFile) > 0);
+        assert(statusInflate == INFLATE_SUCCESS);
+    }
+
+    B63_SUSPEND {
+        FilesAreEqual(pInFile, pOutDecompressedFile);
         fclose(pInFile);
-        fclose(pOutFile);
+        fclose(pOutCompressedFile);
+        fclose(pOutDecompressedFile);
     }
 }
 
@@ -121,8 +217,6 @@ int main(int argc, char **argv) {
     } else {
         g_pFullPathToBenchmarkTestFiles = argv[1];
     }
-    // printf("Full path to benchmark test files: %s\n",
-    //       g_pFullPathToBenchmarkTestFiles);
 
     const int argcForB63 = argc - 1;
     argv[1] = argv[0];
---
+++ CoDeLib/Inflate_zlib/src/Inflate_zlib.c
@@ -0,0 +1,97 @@
+#include <CoDeLib/Inflate_zlib/Inflate_zlib.h>
+
+#include <assert.h>
+#include <stdio.h>
+#include <zlib.h>
+
+/*
+ * Based on https://www.zlib.net/zlib_how.html
+ */
+
+#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
+#include <fcntl.h>
+#include <io.h>
+#define SET_BINARY_MODE(file) _setmode(_fileno(file), O_BINARY)
+#else
+#define SET_BINARY_MODE(file)
+#endif
+
+#define BUFFER_SIZE_BYTES 16384
+
+INFLATE_RETURN_CODES Inflate(FILE *pInputFile, FILE *pOutputFile,
+                             void *pOptions __attribute__((unused))) {
+    if (pInputFile == NULL || pOutputFile == NULL) {
+        printf("Invalid input or output file\n");
+        return INFLATE_ERROR;
+    }
+
+    int returnCode;
+    unsigned totalDataReturned;
+    z_stream tmpDataStream;
+    unsigned char inBuffer[BUFFER_SIZE_BYTES];
+    unsigned char outBuffer[BUFFER_SIZE_BYTES];
+
+    /* allocate inflate state */
+    tmpDataStream.zalloc = Z_NULL;
+    tmpDataStream.zfree = Z_NULL;
+    tmpDataStream.opaque = Z_NULL;
+    tmpDataStream.avail_in = 0;
+    tmpDataStream.next_in = Z_NULL;
+    returnCode = inflateInit(&tmpDataStream);
+    if (returnCode != Z_OK) {
+        return INFLATE_ERROR;
+    }
+
+    /* decompress until deflate stream ends or end of file */
+    do {
+        tmpDataStream.avail_in =
+            fread(inBuffer, 1, BUFFER_SIZE_BYTES, pInputFile);
+        if (ferror(pInputFile)) {
+            (void)inflateEnd(&tmpDataStream);
+            return INFLATE_ERROR;
+        }
+        if (tmpDataStream.avail_in == 0) {
+            break;
+        }
+        tmpDataStream.next_in = inBuffer;
+
+        /* run inflate() on input until output buffer not full */
+        do {
+            tmpDataStream.avail_out = BUFFER_SIZE_BYTES;
+            tmpDataStream.next_out = outBuffer;
+
+            returnCode = inflate(&tmpDataStream, Z_NO_FLUSH);
+            assert(returnCode != Z_STREAM_ERROR); /* state not clobbered */
+            switch (returnCode) {
+            case Z_NEED_DICT: {
+                returnCode = Z_DATA_ERROR; /* and fall through */
+                __attribute__((fallthrough));
+            }
+            case Z_DATA_ERROR:
+            case Z_MEM_ERROR: {
+                (void)inflateEnd(&tmpDataStream);
+                return INFLATE_ERROR;
+            }
+            }
+
+            totalDataReturned = BUFFER_SIZE_BYTES - tmpDataStream.avail_out;
+            if (fwrite(outBuffer, 1, totalDataReturned, pOutputFile) !=
+                    totalDataReturned ||
+                ferror(pOutputFile)) {
+                (void)inflateEnd(&tmpDataStream);
+                return INFLATE_ERROR;
+            }
+
+        } while (tmpDataStream.avail_out == 0);
+
+        /* done when inflate() says it's done */
+    } while (returnCode != Z_STREAM_END);
+
+    /* clean up and return */
+    (void)inflateEnd(&tmpDataStream);
+    return returnCode == Z_STREAM_END ? INFLATE_SUCCESS : INFLATE_ERROR;
+}
+
+const struct IInflate inflate_zlib = {
+    .Inflate = Inflate,
+};
